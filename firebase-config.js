b5fc2c9d53fb0b3928c8629d9481d040157b0eb1
ariafffff <petertory15@gmail.com>
2025-06-23 22:23:26 +0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA-KnSv_zTLaXNY3gDjMTZ8SSAS-3Ij0bg",
+  authDomain: "pro-ecommerce-aria.firebaseapp.com",
+  databaseURL: "https://pro-ecommerce-aria-default-rtdb.firebaseio.com",
+  projectId: "pro-ecommerce-aria",
+  storageBucket: "pro-ecommerce-aria.appspot.com",
+  messagingSenderId: "34678804298",
+  appId: "1:34678804298:web:0019c3cbebfc5456114e7a"
+};
+firebase.initializeApp(firebaseConfig);
